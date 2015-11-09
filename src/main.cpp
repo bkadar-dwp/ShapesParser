e149bb338b48580f2cb61a339a61cf35e6454bfd
Balázs Kádár <Balzs.Kdr.ext@dwpbank.de>
2015-11-09 18:17:28 +0100

    read input from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include "IShapeParser.h"
 #include "Shapes.h"
 
-int main()
+#include <fstream>
+
+int main(int argc, char *argv[])
 {
+	if(argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <inFile>\n";
+		return 1;
+	}
+	std::ifstream str(argv[1]);
 	auto parser = IShapeParser::createParser();
-	auto shapes = parser->parseShapes(std::cin);
+	auto shapes = parser->parseShapes(str);
 	std::cout << "Parsed the following shapes: \n";
 	for(auto const &shape : shapes)
 	{
